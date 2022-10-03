f2ca69cdc816da965fc47b8ed851ec20a1f2f2b2
vince <vincent.garret@lapiscine.pro>
2022-10-03 15:24:31 +0200

    timer js

---
+++ scripts/timer.js
@@ -0,0 +1,40 @@
+var timerView = document.getElementById('timerView');
+var start = document.getElementById('strt');
+var stop = document.getElementById('stp');
+var reset = document.getElementById('rst');
+var sec = 0;
+var min = 0;
+var hrs = 0;
+var t;
+
+function tick(){
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
+        }
+    }
+}
+function add() {
+    tick();
+    timerView.textContent = (hrs > 9 ? hrs : "0" + hrs) 
+        	 + ":" + (min > 9 ? min : "0" + min)
+       		 + ":" + (sec > 9 ? sec : "0" + sec);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
+start.onclick = timer;
+stop.onclick = function() {
+    console.log(t)
+    clearTimeout(t);
+}
+reset.onclick = function() {
+    timerView.textContent = "00:00:00";
+    sec = 0; min = 0; hrs = 0;
+}
