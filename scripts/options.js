d2173582d13e7ecc9c0d1b2c1d44815d554171f1
Thomas Vigneron <thomas.vigneron@consulting-for.edenred.com>
2022-10-04 09:11:42 +0200

    Add darkmode

--- scripts/options.js
+++ scripts/options.js
@@ -4,33 +4,33 @@ const hourPerDay = document.getElementById("hour-per-day");
 const logoutButton = document.getElementById("logout-button");
 
 submitSettings.addEventListener("click", (e) => {
-    let hourPerDayValue = hourPerDay.value;
-    // e.preventDefault();
-    storeSettings(hourPerDayValue);
+  let hourPerDayValue = hourPerDay.value;
+  // e.preventDefault();
+  console.log("Test store settings");
+  storeSettings(hourPerDayValue);
 });
 
 function storeSettings(hourPerDayValue) {
-    chrome.storage.local.set({ hourPerDay: hourPerDayValue }, function() {
-        // console.log("Value is set to " + hourPerDayValue)
-    });
+  chrome.storage.local.set({ hourPerDay: hourPerDayValue }, function () {
+    // console.log("Value is set to " + hourPerDayValue)
+  });
 }
 
-window.addEventListener('load', (event) => {
-    if (chrome.storage.local.get(['hourPerDay'])) {
-        chrome.storage.local.get(['hourPerDay'], function(result) {
-            hourPerDay.value = result.hourPerDay;
-        });
-    }
-  });
-
-  /* *********************************
-            BOUTON LOGOUT
-  ********************************* */ 
-  logoutButton.addEventListener("click", (e) => {
-    e.preventDefault();
-  
-    chrome.runtime.sendMessage({ message: "logout" }, function (response) {
-      if (response === "success") window.location.replace("./login.html");
+window.addEventListener("load", (event) => {
+  if (chrome.storage.local.get(["hourPerDay"])) {
+    chrome.storage.local.get(["hourPerDay"], function (result) {
+      hourPerDay.value = result.hourPerDay;
     });
+  }
+});
+
+/* *********************************
+            BOUTON LOGOUT
+  ********************************* */
+logoutButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  chrome.runtime.sendMessage({ message: "logout" }, function (response) {
+    if (response === "success") window.location.replace("./login.html");
   });
-  
+});
